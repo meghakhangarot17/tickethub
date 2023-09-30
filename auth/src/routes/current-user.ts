a8a7f82e360e106a6cff6e1ded1bd7a256c70455
meghakhangarot <meghakhangarot17@gmail.com>
2023-09-30 17:03:11 +0530

    Initial commit

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+import { currentUser } from '../middlewares/current-user';
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', currentUser,(req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+  // if(!req.session?.jwt){
+  //   return res.send({ currentUser:null});
+  // }
+
+  // try{
+  //   const payload = jwt.verify(
+  //     req.session.jwt, 
+  //     process.env.JWT_KEY!
+  //     );
+  //     res.send({ currentUser: payload });
+  //   }
+  //   catch(err){
+  //     res.send({ currentUser:null });
+  //   }
+
+});
+
+export { router as currentUserRouter };
+
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,87 @@
+import express, {Request, Response} from 'express';
+import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
+import { validateRequest } from '../middlewares/validate-request';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
+// import { DatabaseConnectionError } from '../errors/database-connection-error';
+
+const router = express.Router();
+
+router.post('/api/users/signup',[
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters'),
+],
+validateRequest,
+
+async (req: Request, res: Response) => {
+  // const errors = validationResult(req);
+
+  // if(!errors.isEmpty()){
+  //   throw new RequestValidationError(errors.array());
+  // }
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if(existingUser){
+      throw new BadRequestError('Email in use');
+      // console.log('Email in use');
+      // return res.send({});
+    }
+
+    const user = User.build({ email, password});
+    await user.save();
+
+    //Generate a JSON web token(JWT)
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    },
+    process.env.JWT_KEY!
+    // 'asdf'
+    );
+
+    //Store it on session object
+    req.session = {
+      jwt: userJwt
+    };
+
+
+    res.status(201).send(user);
+
+
+
+    //this is javascript (not ts)
+    // const error = new Error('Inavlid email or password');
+    // error.reasons = errors.array();
+    // throw error;
+
+    // throw new Error('Invalid email or password');
+    // return res.status(400).send(errors.array());
+  
+
+  // const { email, password } = req.body;
+
+  // console.log('Creating a user...');
+  // throw new DatabaseConnectionError();
+  // // throw new Error('Error connecting to database');
+
+
+  // res.send({});
+
+  // if(!email || typeof email !== 'string'){
+  //   res.status(400).send('Provide a valid email');
+  // }
+
+  //new user({email, password})
+}
+);
+
+export { router as signupRouter };
